382d2dc732ea3e8b5e66dff347fe376777fa4cc6
LeanderGlanda <leander.wollersberger@gmx.at>
2025-05-22 18:44:50 +0200

    Add mute method for speaker outputs

--- src/drivers/tpa3116d2.rs
+++ src/drivers/tpa3116d2.rs
@@ -20,8 +20,12 @@ impl<'a> TPA3116D2<'a> {
         Ok(())
     }
 
-    #[allow(dead_code)]
-    pub fn mute_channel(&self, _channel: u8) -> Result<(), anyhow::Error> {
+    pub fn mute_speaker_outputs(&self, muted: bool) -> Result<(), anyhow::Error> {
+        let mut i2c = self.i2c.lock().expect("Failed to lock I2C driver");
+        i2c.write(0x42, &[0x4, muted as u8], BLOCK)?;
+        i2c.write(0x42, &[0x5, muted as u8], BLOCK)?;
+        std::thread::sleep(Duration::from_millis(1));
         Ok(())
     }
+    
 }
